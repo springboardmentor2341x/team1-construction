b34d5bdd5db6e038c2450f848606642b20a6ff62
Logeshwaran V <logeshwaranv19@gmail.com>
2026-08-04 12:36:35 +0530

    Delete frontend directory

--- frontend/src/app/core/models/role.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum UserRole {
-  ADMINISTRATOR = 'Administrator',
-  PROJECT_MANAGER = 'Project Manager',
-  SITE_ENGINEER = 'Site Engineer',
-  CONTRACTOR = 'Contractor',
-  WORKER = 'Worker',
-  CLIENT = 'Client'
-}
